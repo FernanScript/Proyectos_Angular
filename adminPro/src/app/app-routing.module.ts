8723684c61fb10f06d5d5610308453b8cae8268e
Fernando <fernando.trejos10g@gmail.com>
2024-01-01 22:11:24 -0500

    add routes and styles

--- adminPro/src/app/app-routing.module.ts
+++ adminPro/src/app/app-routing.module.ts
@@ -1,9 +1,34 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { Grafica1Component } from './pages/grafica1/grafica1.component';
+import { LoginComponent } from './auth/login/login.component';
+import { NotPageFoundComponent } from './pages/not-page-found/not-page-found.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { RouterModule, Routes } from '@angular/router';
+import { PagesComponent } from './pages/pages.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: PagesComponent,
+    children: [
+      { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+      { path: 'dashboard', component: DashboardComponent },
+      { path: 'progress', component: ProgressComponent },
+      { path: 'grafica1', component: Grafica1Component }
+    ]
+  },
+
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+
+  { path: '**', component: NotPageFoundComponent },
+];
 
 @NgModule({
+  declarations: [],
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
