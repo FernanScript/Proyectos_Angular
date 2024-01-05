57e7e1791a3ee9f5d494a2db7ba8765db72e4ef4
Fernando <fernando.trejos10g@gmail.com>
2024-01-04 23:02:30 -0500

    modules create

--- adminPro/src/app/app-routing.module.ts
+++ adminPro/src/app/app-routing.module.ts
@@ -4,12 +4,15 @@ import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { Grafica1Component } from './pages/grafica1/grafica1.component';
 import { LoginComponent } from './auth/login/login.component';
 import { NotPageFoundComponent } from './pages/not-page-found/not-page-found.component';
+import { PagesComponent } from './pages/pages.component';
 import { ProgressComponent } from './pages/progress/progress.component';
 import { RegisterComponent } from './auth/register/register.component';
 import { RouterModule, Routes } from '@angular/router';
-import { PagesComponent } from './pages/pages.component';
 
 const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+
   {
     path: '',
     component: PagesComponent,
@@ -21,14 +24,10 @@ const routes: Routes = [
     ]
   },
 
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
-
   { path: '**', component: NotPageFoundComponent },
 ];
 
 @NgModule({
-  declarations: [],
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
